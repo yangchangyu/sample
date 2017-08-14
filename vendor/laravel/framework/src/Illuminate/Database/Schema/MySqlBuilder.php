4ae89b8d1526e747529fcfaf75145c75d43d2256
yangcy <yangcy291@gmail.com>
2017-08-14 03:07:20 +0000

    init

---
+++ vendor/laravel/framework/src/Illuminate/Database/Schema/MySqlBuilder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Illuminate\Database\Schema;
+
+class MySqlBuilder extends Builder
+{
+    /**
+     * Determine if the given table exists.
+     *
+     * @param  string  $table
+     * @return bool
+     */
+    public function hasTable($table)
+    {
+        $table = $this->connection->getTablePrefix().$table;
+
+        return count($this->connection->select(
+            $this->grammar->compileTableExists(), [$this->connection->getDatabaseName(), $table]
+        )) > 0;
+    }
+
+    /**
+     * Get the column listing for a given table.
+     *
+     * @param  string  $table
+     * @return array
+     */
+    public function getColumnListing($table)
+    {
+        $table = $this->connection->getTablePrefix().$table;
+
+        $results = $this->connection->select(
+            $this->grammar->compileColumnListing(), [$this->connection->getDatabaseName(), $table]
+        );
+
+        return $this->connection->getPostProcessor()->processColumnListing($results);
+    }
+
+    /**
+     * Drop all tables from the database.
+     *
+     * @return void
+     */
+    public function dropAllTables()
+    {
+        $this->disableForeignKeyConstraints();
+
+        foreach ($this->connection->select('SHOW FULL TABLES WHERE table_type = \'BASE TABLE\'') as $table) {
+            $this->drop(get_object_vars($table)[key($table)]);
+        }
+
+        $this->enableForeignKeyConstraints();
+    }
+}
